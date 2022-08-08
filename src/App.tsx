185eeb753de2cdb21f1852cf02185b931c747325
DHN Chandan <php.chandan@gmail.com>
2022-08-09 02:22:20 +0600

    feat: Implement and test typesafe-routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react';
 import {booleanType, hashValues, numberType, route} from "react-router-typesafe-routes/dom";
+import { route as tRoute, stringParser } from "typesafe-routes";
 
 // React-Router-Typesafe-Routes
 export const ROUTES = {
@@ -18,14 +19,23 @@ export const ROUTES = {
                         "users"
                     )
                 }
-            )
+            ),
         }
     ),
 };
 
+
+export const addressRoute = tRoute("address", {}, {})
+export const detailsRoute = tRoute("/details", {}, {addressRoute})
+export const accountRoute = tRoute("/account/:accountId", {
+    accountId: stringParser
+}, {detailsRoute, addressRoute});
+
 function App() {
     // React-Router-Typesafe-Routes
     useEffect(() => {
+        console.log("==========  RRTR ==========")
+
         // /product/:id
         console.log(ROUTES.PRODUCT.path)
         // /product/:id/details
@@ -41,6 +51,21 @@ function App() {
         console.log(ROUTES.PRODUCT.buildUrl({id: 45}, {sectionsCount: 5}, "more"))
         // /product/45/details/users?sectionsCount=5#about
         console.log(ROUTES.PRODUCT.DETAILS.USERS.buildUrl({id: 45}, {sectionsCount: 5}, "about"))
+    }, [])
+
+    // Typesafe Routes
+    useEffect(() => {
+        console.log("===========  TR ===========")
+
+        console.log(accountRoute.template)
+        console.log(accountRoute({accountId: "sdsd45Trd3"}).$)
+        console.log(accountRoute({accountId: "34jlksdj"}).detailsRoute({}).$)
+        console.log(accountRoute({accountId: "swdsd8"}).addressRoute({}).$)
+        console.log(detailsRoute({}).$)
+        console.log(detailsRoute({}).addressRoute({}).$)
+        console.log(addressRoute({}).$)
+
+        console.log(accountRoute.parseParams({accountId: "sd78"}))
     }, [])
 
     return (
